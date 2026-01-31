2a531c81a18ba8e5883c9c2e9f89075db6d6f153
joeycatic <joeybennett3804@gmail.com>
2026-01-31 18:33:30 +0100

    Role maintenence mode view

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,20 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
 
 const MAINTENANCE_FLAG = "1";
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   if (process.env.MAINTENANCE_MODE !== MAINTENANCE_FLAG) {
+    return NextResponse.next();
+  }
+
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+  const role = token?.role;
+  if (role === "ADMIN" || role === "STAFF") {
     return NextResponse.next();
   }
 
